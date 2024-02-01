980b6fee670770d7a3889194c2737b4e73e0937e
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-02-01 18:06:59 +0545

    Two way data binding complete.

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  // name="Deepak Maharjan"
+
+  addToCart: number = 0;
+
+  product = {
+    name: 'iPhone 13',
+    price: 999,
+    color: 'white',
+    discounted: 7.5, //in percentage
+    inStock: 5,
+    pImage: '/assets/image/iphone.jpg'
+  }
+
+  getDiscountedPrice(){
+    return this.product.price - (this.product.price * this.product.discounted / 100);
+  }
+  
+  onChangeName(event: any){
+    // this.name = event.target.value;
+    // console.log(event.target.value);
+  }
+ 
+  // Event binding for click event  
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+  }
+
+  // name: string = 'iPhone 16 pro max';
+  // price: number = 999;
+  // color: string = 'Red';
+  // discounted_price: number = 888;
+  
+
+}
