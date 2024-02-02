13dae574a9d9b598bd3a8968b4910be11dff8906
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-02-02 14:05:42 +0545

    use ngFor directive on main menu to list dynamic menu list.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,39 +7,44 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
-  // name="Deepak Maharjan"
-
-  addToCart: number = 0;
-
-  product = {
-    name: 'iPhone 13',
-    price: 999,
-    color: 'white',
-    discounted: 7.5, //in percentage
-    inStock: 5,
-    pImage: '/assets/image/iphone.jpg'
-  }
-
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discounted / 100);
-  }
+  listOfString: string[] = ['Ram', 'Harry', 'Potato', 'Haku', 'Deepak'];
   
-  onChangeName(event: any){
-    // this.name = event.target.value;
-    // console.log(event.target.value);
-  }
+
+
+
+  // // name="Deepak Maharjan"
+
+  // addToCart: number = 0;
+
+  // product = {
+  //   name: 'iPhone 13',
+  //   price: 999,
+  //   color: 'white',
+  //   discounted: 7.5, //in percentage
+  //   inStock: 5,
+  //   pImage: '/assets/image/iphone.jpg'
+  // }
+
+  // getDiscountedPrice(){
+  //   return this.product.price - (this.product.price * this.product.discounted / 100);
+  // }
+  
+  // onChangeName(event: any){
+  //   // this.name = event.target.value;
+  //   // console.log(event.target.value);
+  // }
  
-  // Event binding for click event  
-  decrementCartValue(){
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-  }
+  // // Event binding for click event  
+  // decrementCartValue(){
+  //   if(this.addToCart > 0){
+  //     this.addToCart--;
+  //   }
+  // }
+  // incrementCartValue(){
+  //   if(this.addToCart < this.product.inStock){
+  //     this.addToCart++;
+  //   }
+  // }
 
   // name: string = 'iPhone 16 pro max';
   // price: number = 999;
