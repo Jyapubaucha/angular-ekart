16cf586cc53d1d76b238241edd3f5a4a99298bfa
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-02-07 19:38:35 +0545

    Search is done using viewChild decorator and passing template variable #searchInput.

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,15 +11,17 @@ export class SearchComponent {
   //Create an event
   @Output()
   searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  @ViewChild('searchInput') searchInputEl: ElementRef;
   
   noSearchTextChanged(){
   }
 
 
-  updateSearchText(inputEL: HTMLInputElement) {
+  updateSearchText() {
     // console.log(inputEL.value);
     // this.searchText = event.target.value;  
-    this.searchText = inputEL.value;
+    this.searchText = this.searchInputEl.nativeElement.value;
     this.searchTextChanged.emit(this.searchText);
   }
 }
